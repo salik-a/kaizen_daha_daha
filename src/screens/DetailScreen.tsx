ea3441ea024f3155a2d95d45018a241940094c53
salik-a <salika@mef.edu.tr>
2024-08-04 09:34:56 +0300

    detail screen ui updated

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,15 +1,27 @@
 import React, { FC } from "react"
-import { ActivityIndicator, ViewStyle } from "react-native"
-import { AppStackScreenProps } from "src/navigators"
-import { Screen, Text } from "src/components"
+import {
+  ActivityIndicator,
+  View,
+  ViewStyle,
+  Image,
+  TextStyle,
+  ImageStyle,
+  Dimensions,
+  ScrollView,
+  Pressable,
+} from "react-native"
+import { AppStackScreenProps, goBack } from "src/navigators"
+import { Button, Icon, Screen, Text } from "src/components"
 import { useQuery } from "@tanstack/react-query"
 import { api, IPromotionDetail } from "../services/api"
-import { spacing } from "src/theme"
+import { colors, spacing } from "src/theme"
 import { translate } from "../i18n"
+
+const { height, width } = Dimensions.get("window")
 
 interface DetailScreenProps extends AppStackScreenProps<"DetailScreen"> {}
 
-const DetailScreen: FC<DetailScreenProps> = ({ navigation, route }) => {
+const DetailScreen: FC<DetailScreenProps> = ({ route }) => {
   const {
     data: promotionDetailData,
     error: promotionDetailError,
@@ -19,6 +31,10 @@ const DetailScreen: FC<DetailScreenProps> = ({ navigation, route }) => {
     queryFn: () => api.getPromotionDetail(Number(route?.params?.Id) ?? 0),
     enabled: true,
   })
+
+  const stripHtmlTags = (html: string) => {
+    return html.replace(/<\/?[^>]+(>|$)/g, "")
+  }
 
   if (promotionDetailLoading) {
     return (
@@ -47,8 +63,44 @@ const DetailScreen: FC<DetailScreenProps> = ({ navigation, route }) => {
   }
 
   return (
-    <Screen style={$root} preset="scroll">
-      <Text text="Detail" />
+    <Screen style={$root} preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$container}>
+      <Pressable style={$backButton} onPress={() => goBack()}>
+        <Icon icon="back" color={colors.white} size={30} />
+      </Pressable>
+      {promotionDetailData && (
+        <>
+          <ScrollView style={{ height: height }}>
+            <View style={$topContainer}>
+              <Image
+                source={{ uri: promotionDetailData.ImageUrl }}
+                resizeMode="cover"
+                style={[$image]}
+              />
+              <View style={$topInner}>
+                <View style={$logoImageContainer}>
+                  <Image
+                    source={{ uri: promotionDetailData.BrandIconUrl }}
+                    resizeMode="contain"
+                    style={$brandIconImage}
+                  />
+                </View>
+                <View style={$remainingTextContainer}>
+                  <Text style={$remainingText}>{promotionDetailData.RemainingText}</Text>
+                </View>
+              </View>
+            </View>
+            <View style={$content}>
+              <Text style={$title}>{stripHtmlTags(promotionDetailData.Title)}</Text>
+              <Text style={$description}>{stripHtmlTags(promotionDetailData.Description)}</Text>
+            </View>
+          </ScrollView>
+          <Button
+            text={promotionDetailData.DetailButtonText}
+            textStyle={$buttonTextStyle}
+            style={$buttonStyles}
+          />
+        </>
+      )}
     </Screen>
   )
 }
@@ -60,7 +112,98 @@ const $root: ViewStyle = {
 const $container: ViewStyle = {
   paddingTop: spacing.sm,
   paddingBottom: spacing.sm,
-  paddingHorizontal: spacing.xs,
+}
+
+const $backButton: ViewStyle = {
+  backgroundColor: colors.grayScale,
+  borderRadius: 50,
+  padding: 10,
+  width: 40,
+  height: 40,
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  left: 10,
+  top: 40,
+  zIndex: 100,
+}
+
+const $topContainer: ViewStyle = {
+  width: width,
+  alignItems: "center",
+}
+
+const $topInner: ViewStyle = {
+  position: "absolute",
+  bottom: -18,
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: "90%",
+}
+
+const $image: ImageStyle = {
+  height: height * 0.5,
+  width: width,
+  borderBottomLeftRadius: 120,
+}
+
+const $logoImageContainer: ViewStyle = {
+  backgroundColor: colors.white,
+  padding: 8,
+  borderRadius: 50,
+}
+
+const $brandIconImage: ImageStyle = {
+  width: 75,
+  height: 75,
+  borderRadius: 50,
+}
+
+const $content: ViewStyle = {
+  alignItems: "center",
+}
+
+const $title: TextStyle = {
+  fontSize: 24,
+  fontWeight: "bold",
+  color: "#333",
+  marginTop: 25,
+  paddingHorizontal: 4,
+}
+
+const $remainingTextContainer: ViewStyle = {
+  borderRadius: 15,
+  backgroundColor: colors.black,
+  paddingHorizontal: 10,
+  paddingVertical: 8,
+}
+
+const $remainingText: TextStyle = {
+  fontSize: 16,
+  color: colors.white,
+}
+
+const $description: TextStyle = {
+  fontSize: 16,
+  color: colors.black,
+  marginTop: 20,
+  paddingHorizontal: 8,
+}
+
+const $buttonTextStyle: TextStyle = {
+  color: colors.white,
+}
+
+const $buttonStyles: ViewStyle = {
+  backgroundColor: colors.buttonColor,
+  borderColor: colors.buttonColor,
+  borderRadius: 25,
+  width: width - 40,
+  height: 50,
+  alignSelf: "center",
+  position: "absolute",
+  bottom: 75,
 }
 
 export default DetailScreen
--- src/services/api/api.types.ts
+++ src/services/api/api.types.ts
@@ -45,9 +45,31 @@ export interface IPromotions {
 }
 
 export interface IPromotionDetail {
-  IconUrl: string
+  DetailButtonText: string | undefined
+  BrandIconColor: string
+  Description: string
+  BrandIconUrl: string
+  BrandPromotionCardParticipationText: string
+  CardType: string
+  ExternalLoginGate: any
+  ExternalRedirectType: any
+  ExternalType: any
+  ExternalUrl: any
+  ExternalWebviewType: any
   Id: number
-  Name: string
-  Rank: number
+  ImageUrl: string
+  IsLuckyDay: boolean
+  ListButtonText: string
+  ListButtonTextBackGroudColor: string
+  LuckyDayBackgroundColor: string
+  LuckyDayText: string
+  LuckyDayTextColor: string
+  PromotionCardColor: string
+  RemainingText: string
+  ScenarioType: string
+  SeoName: string
+  Title: string
+  Unavailable: boolean
+  Unvisible: boolean
 }
 
